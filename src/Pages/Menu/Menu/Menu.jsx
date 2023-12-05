555e557fbd6a300dd6e15abadbbe8ea031df4139
nosianuri <nurinosia@gmail.com>
2023-12-06 01:27:57 +0600

    add custom hooks usermenu

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/menu-bg.png';
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 
 const Menu = () => {
     return (
@@ -10,7 +10,7 @@ const Menu = () => {
             <title>Nectar Nook | Menu</title>
         </Helmet>
         <Cover img={menuImg} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
+            
         </div>
     );
 };
