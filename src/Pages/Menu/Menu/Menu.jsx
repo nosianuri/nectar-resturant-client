84db8491a26569ff16cf8f1bd7155168113934b7
nosianuri <nurinosia@gmail.com>
2023-12-06 00:47:01 +0600

    add dynamic page title using react helmet

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+
+const Menu = () => {
+    return (
+        <div>
+        <Helmet>
+            <title>Nectar Nook | Menu</title>
+        </Helmet>
+            <h2>My menu</h2>
+        </div>
+    );
+};
+
+export default Menu;
